6f55b0d93c1201bdaafd443a16bd612847eefb7f
danieladbsnt <daniela.darnea@bosonit.com>
2023-01-02 17:22:12 +0100

    tried input in ts and added ngfor in html

--- crud/src/app/table/table.component.ts
+++ crud/src/app/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { User } from '../interfaces/user';
 //import {MatTableModule} from '@angular/material/table';
@@ -10,6 +10,44 @@ import { User } from '../interfaces/user';
 })
 export class TableComponent {
 
-  constructor() { }
+  @Input() formValues!: User[];
+
+public users: User[] = [
+  {
+    username: 'aaa',
+    password: 'aaa',
+    confirmPass: 'aaa',
+    email: 'aaa@mail.com',
+    subscribed: false,
+    country: 'rumania',
+    city: 'medias',
+    id:1
+  },
+  {
+    username: 'bbb',
+    password: 'aaa',
+    confirmPass: 'aaa',
+    email: 'bbb@mail.com',
+    subscribed: false,
+    country: 'austria',
+    city: 'viena',
+    id: 2
+  }
+];
+
+  constructor() { 
+    console.log(this.formValues);
+    
+  }
+
+edit() {
+  console.log('editando');
+  
+}
+
+delete() {
+  console.log( 'borrando');
+
+}
 
 }
