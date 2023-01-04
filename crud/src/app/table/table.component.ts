18d1b7c65d4637873c2638bdd2b84f652f155ac6
danieladbsnt <daniela.darnea@bosonit.com>
2023-01-04 11:35:48 +0100

    added update and delete functions but works only in console and when refreshing page

--- crud/src/app/table/table.component.ts
+++ crud/src/app/table/table.component.ts
@@ -9,40 +9,36 @@ import { ServiceService } from '../services/service.service';
 })
 export class TableComponent implements OnInit{
 
-@Output() data = new EventEmitter<any>();
-
+//@Output() data = new EventEmitter<any>();
 
  users: User[] = [];
 
   constructor(private service: ServiceService) { 
-    
   }
-
 
   ngOnInit(): void {
     this.service.getData()
       .subscribe(users => {
         this.users = users;
         console.log(users);
-        
       });
   }
-
-update() {
+//cuando le de click al lapiz para editar, se tiene que poner la info
+//del user que se toca en el form.
+update(data:any, id: number) {
   console.log('editando');
-  // let body = {
-    
-  // }
-  // this.service.updateData(data, id)
-  // .subscribe(resp => {
-  //   console.log(resp);
-    
-  // })
+  
+  this.service.updateData(data, id)
+  .subscribe(resp => {
+    console.log(resp);
+  
+  })
 }
 
-delete() {
-  console.log( 'borrando');
-  // this.service.deleteData(this.users.id) POR QUE NO VAAA
+delete(id: number) {
+  //borra pero no se pinta en la tabla al menos que refresques.
+  this.service.deleteData(id)
+    .subscribe(resp => {}) 
 }
 
 }
